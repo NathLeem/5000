1229a8f425db671f3e9374c93ae97883954ab029
NathLeem <91312993+NathLeem@users.noreply.github.com>
2022-01-20 03:08:27 -1200

    Création de chaque joueur du jeu
    j'ai demandé a chaque joueur d'indiquer son prénom

---
+++ ProjetFinal_leemans_nathan/ProjetFinal_leemans_nathan/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetFinal_leemans_nathan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string player1;
+            string player2;
+            string player3;
+            string player4;
+            string player5;
+
+            Console.WriteLine("Jeu du 5000");
+            Console.WriteLine("--------------");
+            Console.WriteLine("Veuillez entrer le prénom du premier joueur");
+            player1 = Console.ReadLine();
+            Console.WriteLine("Veuillez entrer le prénom du deuxième joueur");
+            player2 = Console.ReadLine();
+            Console.WriteLine("Veuillez entrer le prénom du troisième joueur");
+            player3 = Console.ReadLine();
+            Console.WriteLine("Veuillez entrer le prénom du quatrième joueur");
+            player4 = Console.ReadLine();
+            Console.WriteLine("Veuillez entrer le prénom du cinquième");
+            player5 = Console.ReadLine();
+            Console.WriteLine("Règles du jeu");
+            Console.WriteLine("////");
+
+            Console.ReadLine();
+        }
+    }
+}
