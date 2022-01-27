d78f71c1c6223d8a7c2b517231f975a1c23dd086
NathLeem <91312993+NathLeem@users.noreply.github.com>
2022-01-27 16:07:17 +0100

    assignation des joueurs
    création d'une fonction qui permet aux utilisateurs de choisir le nombre de joueur qui vont jouer.

--- ProjetFinal_leemans_nathan/ProjetFinal_leemans_nathan/Program.cs
+++ ProjetFinal_leemans_nathan/ProjetFinal_leemans_nathan/Program.cs
@@ -16,22 +16,71 @@ namespace ProjetFinal_leemans_nathan
             string player4;
             string player5;
 
-            Console.WriteLine("Jeu du 5000");
-            Console.WriteLine("--------------");
-            Console.WriteLine("Veuillez entrer le prénom du premier joueur");
-            player1 = Console.ReadLine();
-            Console.WriteLine("Veuillez entrer le prénom du deuxième joueur");
-            player2 = Console.ReadLine();
-            Console.WriteLine("Veuillez entrer le prénom du troisième joueur");
-            player3 = Console.ReadLine();
-            Console.WriteLine("Veuillez entrer le prénom du quatrième joueur");
-            player4 = Console.ReadLine();
-            Console.WriteLine("Veuillez entrer le prénom du cinquième");
-            player5 = Console.ReadLine();
+
+            Console.WriteLine("-------------");
+            Console.Write("|");
+            Console.Write("Jeu du 5000");
+            Console.WriteLine("|");
+            Console.WriteLine("-------------");
             Console.WriteLine("Règles du jeu");
             Console.WriteLine("////");
 
+            ChoixNbreJoueur(player1,player2,player3,player4,player5);
+            
             Console.ReadLine();
+        }
+        static void ChoixNbreJoueur(int NbreJoueur)
+        {
+            string player1;
+            string player2;
+            string player3;
+            string player4;
+            string player5;
+            
+
+            Console.WriteLine("Combien de joueurs vont jouer ?");
+            NbreJoueur = int.Parse(Console.ReadLine());
+
+            if (NbreJoueur == 2)
+            {
+                Console.WriteLine("Veuillez entrer le prénom du premier joueur");
+                player1 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du deuxième joueur");
+                player2 = Console.ReadLine();
+            }
+            if (NbreJoueur == 3)
+            {
+                Console.WriteLine("Veuillez entrer le prénom du premier joueur");
+                player1 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du deuxième joueur");
+                player2 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du troisième joueur");
+                player3 = Console.ReadLine();
+            }
+            if (NbreJoueur == 4)
+            {
+                Console.WriteLine("Veuillez entrer le prénom du premier joueur");
+                player1 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du deuxième joueur");
+                player2 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du troisième joueur");
+                player3 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du quatrième joueur");
+                player4 = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Veuillez entrer le prénom du premier joueur");
+                player1 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du deuxième joueur");
+                player2 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du troisième joueur");
+                player3 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du quatrième joueur");
+                player4 = Console.ReadLine();
+                Console.WriteLine("Veuillez entrer le prénom du cinquième joueur");
+                player5 = Console.ReadLine();
+            }
         }
     }
 }
